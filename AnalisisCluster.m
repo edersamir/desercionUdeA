194c23d86df1473fb8a36e0ded052193fdec0922
Eder Samir <edersamir@gmail.com>
2023-04-15 11:05:29 -0500

    Add files via upload

---
+++ AnalisisCluster.m
@@ -0,0 +1,24 @@
+
+pkg load statistics
+pkg load io
+
+clc
+clear
+close all;
+
+cluster=7;
+ubicacionestado=14;
+
+%datos=xlsread(strcat("idx",num2str(cluster),".xlsx"));
+idx=csvread(strcat("idx",num2str(cluster),".csv"));
+datosentrenamiento=xlsread('DatosEntr_Num_normalizados.xlsx');
+tamidx=size(idx);
+MatrizSexo=zeros(max(datosentrenamiento(:,12)+1),max(idx));
+MatrizPrograma=zeros(max(datosentrenamiento(:,13)+1),max(idx));
+%for  j=1:max(idx) 
+   for i=1:tamidx(1)
+      MatrizSexo(datosentrenamiento(i,12)+1,idx(i))=MatrizSexo(datosentrenamiento(i,12)+1,idx(i))+1; 
+      MatrizPrograma(datosentrenamiento(i,13)+1,idx(i))=MatrizPrograma(datosentrenamiento(i,13)+1,idx(i))+1;  
+   endfor 
+  
+%endfor
